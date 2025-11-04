b7bb6fc5b66a9f507ed848d3f54b49326be066e0
JohnGalt-create <paytonvirgil@outlook.com>
2025-11-04 15:53:01 -0500

    Update DashboardPage.jsx

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,19 +1,19 @@
-import React, { useState } from "react";
-import Dashboard from "../components/Dashboard";
+import React, { useState } from 'react';
+import Dashboard from '../components/Dashboard';
 
 export default function DashboardPage() {
   const [budgetData, setBudgetData] = useState({
     mealPlan: 500,
     spending: [
-      { category: "Food", amount: 50 },
-      { category: "Books", amount: 20 },
+      { category: 'Food', amount: 50 },
+      { category: 'Books', amount: 20 },
     ],
   });
 
   return (
     <div className="p-4 pt-6">
       <img
-        src="/buny_logo.png"
+        src="/16e81d160af714dae794b1500f9b3e2e.png"
         alt="Binghamton Logo"
         className="w-32 mx-auto mb-4"
       />
