8a0f0320e3776b65670439c8a9acd7b95396c915
JohnGalt-create <paytonvirgil@outlook.com>
2025-11-04 15:31:56 -0500

    Create BudgetPage.jsx

---
+++ BudgetPage.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import BudgetInput from "../components/BudgetInput";
+
+export default function BudgetPage() {
+  const [budgetData, setBudgetData] = useState({
+    mealPlan: 500,
+    spending: [],
+  });
+
+  return (
+    <div className="p-4 pt-6">
+      <BudgetInput onUpdate={setBudgetData} />
+    </div>
+  );
+}
