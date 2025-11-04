020121316f26d01442e5deb2c31ea09421f99b26
JohnGalt-create <paytonvirgil@outlook.com>
2025-11-04 15:33:20 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  theme: {
-    extend: {},
+theme: {
+  extend: {
+    colors: {
+      bunyGreen: '#006341',
+      bunyBlue: '#0077c8',
+    },
   },
-  plugins: [],
-};
+},
